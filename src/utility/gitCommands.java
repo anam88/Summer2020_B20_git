35f6ad9eac797b2f1455ef0ccf252b0da2a330c1
anam88 <amasroor88@gmail.com>
2020-08-05 20:27:31 -0500

    add changes in gitCommands

--- src/utility/gitCommands.java
+++ src/utility/gitCommands.java
@@ -4,13 +4,95 @@ import java.util.Scanner;
 
 public class gitCommands {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("The git commands");
-        System.out.println("Which command we use to start a new repository?");
-        String comm1 = input.nextLine();
-        System.out.println("Which command we use to add a file to the staging area?");
-        String comm2 = input.nextLine();
-        System.out.println("Which command snapshots the file?");
-        String comm3 = input.nextLine();
+        /*
+        GIT COMMANDS WE LEARN ON 07/29/2020
+
+git config
+
+	 git config –global user.name “[name]”
+
+	 git config –global user.email “[email address]”
+
+	 git config -global password "[password]"
+
+	This command sets the author name and email address respectively to be used with your commits.
+
+==============================================================
+git init-Initialized empty Git repository, to store our code
+
+	 git init [repository name]
+
+	This command is used to start a new repository.
+
+=============================================================
+git add .
+
+	 git add .
+
+	This command adds a file to the staging area.
+
+================================================================
+git commit
+
+	 git commit -m “[ Type in the commit message]”
+
+	This command records or snapshots the file permanently in the version history.
+==================================================================
+GIT COMMANDS WE LEARN ON 08/01/2020
+==================================================================
+
+git status
+
+	git status
+
+	This command lists all the files that have to be committed.
+
+What does nothing to commit, working tree clean?
+
+	all changes have been committed already
+
+==================================================================
+git log
+
+	git log
+
+	This command is used to list the version history for the current branch.
+
+- To go back to directly after using git log command. just enter q on the terminal
+
+=================================================================
+git remote
+
+	git remote add
+
+	This command is used to connect your local repository to the remote server.
+=======================================================================
+git push
+
+	git push
+
+	This command sends the committed changes of master branch to your remote repository.
+===============================================================
+git pull
+
+	git pull
+
+	update your current HEAD branch with the latest changes from the remote server. This means that pull not only downloads new data; it also directly integrates it into your current working copy files.
+==============================================================
+git fetch
+
+	git fetch (new commit from remote repo, added to local repo)
+
+
+	git fetch really only downloads new data from a remote repository - but it doesn't integrate any of this new data into your working files. Fetch is great for getting a fresh view on all the things that happened in a remote repository.
+
+==============================================================
+git merge
+
+	git merge
+
+	This command merges the specified branch’s history into the current branch.
+==============================================================
+         */
     }
 }
